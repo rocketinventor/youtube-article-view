77246dff2887d5826394774ec60ddb044e92b147
Elliot Gerchak <e@dementedlab.com>
2015-09-18 18:09:47 +0000

    clean up some old code

--- youtube.js
+++ youtube.js
@@ -23,12 +23,6 @@ function getContent() {
 
 // parse data and update DOM
 function setContent() {
-  // var content = document.getElementById("content").innerHTML;
-  // document.body.innerHTML = transcript.nodeName;
-  // objtree_xml = transcript;
-  // var xotree_out = xotree.parseXML( objtree_xml );
-  // parseXML(transcript);
-  // console.log(xotree_out);
   document.getElementById("content").innerHTML = transcript.textContent;
   document.getElementById("img").src = "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
   // document.getElementById("link1").href = "tittle1"
@@ -59,44 +53,5 @@ function newURL() {
   } //set to a random id if none found
   getContent();
 }
-
-// // Changes XML to JSON
-// function xmlToJson(xml) {
-
-// 	// Create the return object
-// 	var obj = {};
-
-// 	if (xml.nodeType == 1) { // element
-// 		// do attributes
-// 		if (xml.attributes.length > 0) {
-// 		obj["@attributes"] = {};
-// 			for (var j = 0; j < xml.attributes.length; j++) {
-// 				var attribute = xml.attributes.item(j);
-// 				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-// 			}
-// 		}
-// 	} else if (xml.nodeType == 3) { // text
-// 		obj = xml.nodeValue;
-// 	}
-
-// 	// do children
-// 	if (xml.hasChildNodes()) {
-// 		for(var i = 0; i < xml.childNodes.length; i++) {
-// 			var item = xml.childNodes.item(i);
-// 			var nodeName = item.nodeName;
-// 			if (typeof(obj[nodeName]) == "undefined") {
-// 				obj[nodeName] = xmlToJson(item);
-// 			} else {
-// 				if (typeof(obj[nodeName].push) == "undefined") {
-// 					var old = obj[nodeName];
-// 					obj[nodeName] = [];
-// 					obj[nodeName].push(old);
-// 				}
-// 				obj[nodeName].push(xmlToJson(item));
-// 			}
-// 		}
-// 	}
-// 	return obj;
-// };
 
 window.onhashchange = newURL; //add listener for change in hash
