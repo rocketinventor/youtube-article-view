8fee618db3f163559c692deae32f31b9c337ccfa
Elliot Gerchak <e@dementedlab.com>
2015-09-21 12:00:17 +0000

    improved load order
    now, the JS will wait for the page to load to run. The js running right away before was causing a few intermitent problems.
    Also, the transcript was being fetched first and title and image were only fetched after the transcript had loaded even though you can't see it without scrolling first.

--- youtube.js
+++ youtube.js
@@ -1,17 +1,42 @@
 var id; //make video id variable public
 id = "zzfCVBSsvqA"; //set fallback url if no hash
 var transcript; //another public var
-newURL(); //get url when page loads
+// newURL(); //get url when page loads
 var key = "AIzaSyC6Ymc1aoVbR3zMgsZqFCef9SOoHQV5X0Y";
 
-// get data/content/transcript from internet
+// parse data and update DOM
 function getContent() {
+  $.ajax({
+    url: "https://www.googleapis.com/youtube/v3/videos?id=" + id + "&key=" + key + "&fields=items(snippet)&part=snippet",
+    dataType: "jsonp",
+    success: function(data) {
+      console.log(data.items[0].snippet);
+      document.getElementById("title").innerHTML = data.items[0].snippet.title;
+      document.title = data.items[0].snippet.title + " (YouTube article view)";
+      document.getElementById("author").innerHTML = data.items[0].snippet.channelTitle;
+      setContent();
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      alert(textStatus, +' | ' + errorThrown);
+    }
+  });
+  setContent();
+  document.getElementById("img").src = "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
+  document.getElementById("img").style = "opacity:0";
+  document.getElementById("img").style.transition = "opacity ease-out  1s";
+  document.getElementById("img").style.opacity = "1";
+}
+
+// get data/content/transcript from internet
+function setContent() {
+  // document.getElementById("link1").href = "tittle1"
+  // document.getElementById("link1").text = 
+
   var xhr = new XMLHttpRequest();
   xhr.onload = function() {
     transcript = xhr.responseXML.documentElement;
     document.getElementById("content").innerHTML = transcript.textContent;
     // transcript = xhr.responseXML;
-    setContent();
   };
   xhr.onerror = function() {
     console.log("Error while getting XML.");
@@ -20,31 +45,6 @@ function getContent() {
   xhr.open("GET", url);
   xhr.responseType = "document";
   xhr.send();
-}
-
-// parse data and update DOM
-function setContent() {
-  document.getElementById("img").src = "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
-  document.getElementById("img").style = "opacity:0";
-  document.getElementById("img").style.transition = "opacity ease-out  1s";
-  // document.getElementById("link1").href = "tittle1"
-  // document.getElementById("link1").text = 
-  
-  $.ajax({
-      url: "https://www.googleapis.com/youtube/v3/videos?id=" + id + "&key="+ key + "&fields=items(snippet)&part=snippet", 
-      dataType: "jsonp",
-      success: function(data){
-               console.log(data.items[0].snippet);
-              document.getElementById("title").innerHTML = data.items[0].snippet.title;
-              document.title = data.items[0].snippet.title + " (YouTube article view)";
-              document.getElementById("author").innerHTML = data.items[0].snippet.channelTitle;
-              document.getElementById("img").style.opacity="1";
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-          alert (textStatus, + ' | ' + errorThrown);
-      }
-  });
-  
 }
 
 //get video id and update variabke
@@ -57,4 +57,5 @@ function newURL() {
   getContent();
 }
 
+window.addEventListener("load", newURL);
 window.onhashchange = newURL; //add listener for change in hash
