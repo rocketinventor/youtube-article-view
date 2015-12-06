3705fe8e7ad1f51be3991665b04f1ec7cdfc8b04
Elliot Gerchak <e@dementedlab.com>
2015-12-06 15:19:04 +0000

    added bookmarklet.js
    I finally decided to commit a previously unstaged file called bookmarklet.js which contains the pretty code for the bookmarklet, as well as the minified js in compiled bookmarklet form.

---
+++ bookmarklet.js
@@ -0,0 +1,8 @@
+// pretty js
+var video = document.location.href;
+var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+var match = video.match(regExp);
+document.location = "https://rawgit.com/rocketinventor/youtube-article-view/master/index.html#" + match[2];
+
+// bookmarklet
+javascript:(function(){document.location="https://rawgit.com/rocketinventor/youtube-article-view/master/index.html#"+location.href.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/)[2];})();
